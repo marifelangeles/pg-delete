4a3e19e16264672868f3403e9e671820f179366c
Marifel Angeles <mmarifel@gmail.com>
2019-02-13 12:51:46 -0600

    get data from server and display on table

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,4 +5,36 @@ $(document).ready(docReady);
 function docReady(){
     console.log('jq');
     
+    // display restaurants table on load
+    getRestaurants();
 }
+
+function getRestaurants(){
+    console.log('in getRestaurants');
+    
+    // get data from server
+    $.ajax({
+        url: '/restaurants',
+        method: 'GET'
+    }).then( function(response) {
+        console.log('back from GET', response);
+        // empty table
+        $('#restaurantTableBody').empty();
+        // display restaurants on table
+        response.forEach(restaurant => {
+            console.log('restaurant:', restaurant);
+            // get object values
+            // append values to table
+            $('#restaurantTableBody').append(`
+            <tr>
+                <td>${restaurant.name}</td>
+                <td>${restaurant.type}</td>
+            </tr>
+            `);
+        
+        });
+        
+    }).catch( function(error) {
+        alert('error with GET', error)
+    });
+} // end getRestaurants
